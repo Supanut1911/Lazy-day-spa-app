1d8fec94d05f55c9ad945aa595645744ca9f9d2a
Supanut1911 <ynotmela@gmail.com>
2023-03-19 12:57:34 +0700

    useQuery-for-fetch-treatment-and-handle-fallback-when-not-done-fetching-data

--- src/components/treatments/hooks/useTreatments.ts
+++ src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+const getTreatments = async (): Promise<Treatment[]> => {
+  const { data } = await axiosInstance.get('/treatments');
+  console.log('called');
 
+  return data;
+};
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+  const { data = fallback } = useQuery([queryKeys.treatments], getTreatments);
+  return data;
 }
