36370373134232bb6114a9e9635f7da5ff71e803
Supanut1911 <ynotmela@gmail.com>
2023-04-07 09:27:04 +0700

    add refetchInterval for refetch appointment

--- src/components/appointments/hooks/useAppointments.ts
+++ src/components/appointments/hooks/useAppointments.ts
@@ -110,6 +110,7 @@ export function useAppointments(): UseAppointments {
       refetchOnMount: true,
       refetchOnReconnect: true,
       refetchOnWindowFocus: true,
+      refetchInterval: 30000,
     },
   );
 
