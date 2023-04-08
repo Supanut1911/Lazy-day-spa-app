618bb729e6672a07404f720b679db9fe266316d3
Supanut1911 <ynotmela@gmail.com>
2023-04-08 23:46:42 +0700

    add queryClient-seQueryData for auth

--- src/components/user/hooks/useUser.ts
+++ src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,22 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  console.log('call1');
+  if (!user) return null;
+  console.log('call2');
+
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  console.log('call3');
+
+  console.log('data =>', data);
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -28,16 +35,20 @@ interface UseUser {
 
 export function useUser(): UseUser {
   // TODO: call useQuery to update user data from server
-  const user = null;
+  // const user = null;
+  const queryClinet = useQueryClient();
+  const { data: user } = useQuery([queryKeys.user], () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    queryClinet.setQueryData([queryKeys.user], newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    queryClinet.setQueryData([queryKeys.user], null);
   }
 
   return { user, updateUser, clearUser };
