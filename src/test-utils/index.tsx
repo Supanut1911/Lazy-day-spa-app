6d33df66ad6338f70b8f6bdb459e1a66352c6e67
Supanut1911 <ynotmela@gmail.com>
2023-04-11 15:28:54 +0700

    :hammer: - setup queryClient for test

--- src/test-utils/index.tsx
+++ src/test-utils/index.tsx
@@ -1,11 +1,20 @@
-import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = (): React.FC => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const renderWithQuery = (
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult => {
+  const queryClient = client ?? generateQueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+};
