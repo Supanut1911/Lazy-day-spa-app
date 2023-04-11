cbad580cc0c84414585b8b4cb66dd43f13e14afa
Supanut1911 <ynotmela@gmail.com>
2023-04-12 01:25:59 +0700

    expect treatmentTitle

--- src/components/treatments/tests/Treatments.test.tsx
+++ src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,11 @@ import { renderWithQuery } from 'test-utils';
 
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   renderWithQuery(<Treatments />);
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /Massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
