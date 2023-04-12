ad74b043e8023bc97ea398af3f6bf037983813ca
Supanut1911 <ynotmela@gmail.com>
2023-04-12 09:23:10 +0700

    :hammer: - add test query all staff

--- src/components/staff/tests/AllStaff.test.tsx
+++ src/components/staff/tests/AllStaff.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import { renderWithQuery } from 'test-utils';
 
 // import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
@@ -6,8 +7,13 @@ import { screen } from '@testing-library/react';
 // import { renderWithClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
-test('renders response from query', () => {
-  // write test here
+test('renders response from query', async () => {
+  renderWithQuery(<AllStaff />);
+  const allStaffTitle = await screen.findAllByRole('heading', {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(allStaffTitle).toHaveLength(4);
 });
 
 test('handles query error', async () => {
